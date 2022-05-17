d4bf90905000a2b5c1c8aedf0dba9b08683ebca9
ianaack <ianaack@gmail.com>
2022-05-17 13:40:40 -0400

    Used Module 14 code as starter

--- models/index.js
+++ models/index.js
@@ -1,3 +1,37 @@
+// import all models
+const Post = require("./Post");
 const User = require("./User");
+const Comment = require("./Comment");
 
-module.exports = { User };
+// create associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "SET NULL",
+});
+
+module.exports = { User, Post, Comment };
