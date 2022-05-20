d8db6ad1ddd12a3a29cfa844c8abf7f583a22961
ianaack <ianaack@gmail.com>
2022-05-19 20:25:23 -0400

    Bug: after log in, reloads login, not /dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -79,7 +79,7 @@ router.get("/post/:id", (req, res) => {
 // get login page
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/");
+    res.redirect("/dashboard");
     return;
   }
 
